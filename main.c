b927a53aee3ef2b1396c8894cf3dfb4c624901fb
Sarafa92 <41699020+Sarafa92@users.noreply.github.com>
2018-09-10 18:14:53 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -16,46 +16,85 @@ typedef struct {
 
 int main(){
 	
-	printf("%s", "Ciaone1");
-	printf("%c",'\n');
-
+	
+printf("inizio\n\n");
 	BitMap* bmap = malloc(sizeof(BitMap));
-	int numBits = bmap->num_bits = 23;
+	int numBits = bmap->num_bits = 24;
 	bmap->entries =  malloc(sizeof(char) * numBits );
-		printf("%s", "Ciaone2");
-		printf("%c",'\n');
+		
+
 
 	//Inizializzazione tutti i bit sono free
-		for (int i = 0; i < numBits; i++){
+		for (int i = 0; i < numBits ; i++){
 		bmap->entries[i] = 0;
 	}
-		printf("%s", "Ciaone3");
-		printf("%c",'\n');
-
+		
+	
+	//----SET----
 	BitMap_set(bmap,5,1);
+	BitMap_set(bmap,15,1);
+	BitMap_set(bmap,22,1);
+	BitMap_set(bmap,8,1);
+	
+	//---GET---
+	printf("getttt\n\n");
+	
 	
 	int locazione = BitMap_get(bmap,0,1);
-	printf("%s", "Ciaone");
+	int locazione2 = BitMap_get(bmap,6,1);
+	int locazione3 = BitMap_get(bmap,13,1);
+	int locazione4 = BitMap_get(bmap,16,1);
+	int locazione5 = BitMap_get(bmap,6,0);
+	
+
+	
+	printf("\n\n");
+	printf("Deve trovare 5 -----> %d", locazione);
+	printf("%c",'\n');
+	printf("Deve trovare 8 -----> %d", locazione2);
+	printf("%c",'\n');
+	printf("Deve trovare 15 -----> %d", locazione3);
+	printf("%c",'\n');
+	printf("Deve trovare 22 -----> %d", locazione4);
+	printf("%c",'\n');
+	printf("Deve trovare 6 -----> %d", locazione5);
 	printf("%c",'\n');
 	
-	printf("%d", locazione);
+	//---BLOCKTOINDEX---
+	printf("---------BLOCK TO INDEX---------\n\n");
+	/*int num;
+	printf("\n\nBlock to index, inserisci un indice\n");
+	scanf("%d", &num);*/
+	
+	int num= 18;
+	BitMapEntryKey coppia = BitMap_blockToIndex(num,bmap);
+	
+	printf("dato l'indice %d , otteniamo il byte numero: ", num);
+	printf("%d" "%s" "%d", coppia.entry_num,", bit numero: ", coppia.bit_num);
+	printf("\n\n");
+	
+	//---INDEXTOBLOCK---
+	
+	printf("---------INDEX TO BLOCK---------\n\n");
+	int trovaloc1 = BitMap_indexToBlock(2,7,bmap);
+	
+	printf("Dato il byte 2 e il bit 7 ottengo l'indice numero:  ");
+	printf("%d",trovaloc1);
 	printf("%c",'\n');
 	
-	BitMapEntryKey coppia = BitMap_blockToIndex(18);
+	int trovaloc2 = BitMap_indexToBlock(2,8,bmap);
 	
-	int trovaloc2 = BitMap_indexToBlock(2,3);
-	
-	printf("%s","data locazione 15");
-	printf("%c",'\n');
-	
-	printf("%s","byte numero   ");
-	printf("%d" "%s" "%d", coppia.entry_num," bit numero ", coppia.bit_num);
-	printf("%c",'\n');
-	printf("%s","una sola");
-	printf("%c",'\n');
+	printf("Dato il byte 2 e il bit 8 ottengo l'indice numero:  ");
 	printf("%d",trovaloc2);
 	printf("%c",'\n');
 	
+	int trovaloc3 = BitMap_indexToBlock(2,4,bmap);
+	
+	printf("Dato il byte 0 e il bit 8 ottengo l'indice numero:  ");
+	printf("%d",trovaloc3);
+	printf("%c",'\n');
+	
+
 
 	return 0;
 }
