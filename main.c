cafca6cfcbe721ea9d733fd3b17c1814da4d6b6d
Sarafa92 <41699020+Sarafa92@users.noreply.github.com>
2018-09-23 21:17:50 +0200

    Update main.c
    piccole modifiche

--- main.c
+++ main.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
-#include "bitmap.h"
-#include "bitmap.c"
-#include <stdlib.h>
+#include "disk_driver.c"
 
-/*typedef struct{
-  int num_bits;
-  char* entries;
-}  BitMap;
 
-typedef struct {
-  int entry_num;
-  char bit_num;
-} BitMapEntryKey;
-*/
-
-int main(){
+int main(int agc, char** argv){
 	
+	
+	/*typedef struct{
+	  int num_bits;
+	  char* entries;
+	}  BitMap;
+	
+	typedef struct {
+	  int entry_num;
+	  char bit_num;
+	} BitMapEntryKey;
+	*/
 
 // -----------------TEST BITMAP --------------------------
 
 printf("\n\n\n\n---------------------------------TEST BITMAP ------------------------------------------\n");
-printf("inizio\n\n");
-	
-	BitMap* bmap = malloc(sizeof(BitMap));
-	int numBits = bmap->num_bits = 24;
-	bmap->entries =  malloc(sizeof(char) * numBits );
+	printf("inizio\n\n");
+		BitMap* bmap = malloc(sizeof(BitMap));
+		int numBits;
+		bmap->num_bits = 24;
+		numBits = bmap->num_bits;
+		bmap->entries =  malloc(sizeof(char) * numBits );
 		
+		int num_blocks_bmap = numBits/8;
+		printf("La mia bitmap ha '%d' blocchi\n\n", num_blocks_bmap);
+		
+
+
 	//Inizializzazione tutti i bit sono free
 		for (int i = 0; i < numBits ; i++){
 		bmap->entries[i] = 0;
 	}
 		
+	
 	//----SET----
 	printf("SET\n\n");
 	BitMap_set(bmap,5,1);
@@ -40,11 +46,15 @@ printf("inizio\n\n");
 	
 	//---GET---
 	printf("GET\n\n");
+	
+	
 	int locazione = BitMap_get(bmap,0,1);
 	int locazione2 = BitMap_get(bmap,6,1);
 	int locazione3 = BitMap_get(bmap,13,1);
 	int locazione4 = BitMap_get(bmap,16,1);
 	int locazione5 = BitMap_get(bmap,6,0);
+	
+
 	
 	printf("\n\n");
 	printf("Deve trovare 5 -----> %d", locazione);
@@ -64,7 +74,8 @@ printf("inizio\n\n");
 	printf("\n\nBlock to index, inserisci un indice\n");
 	scanf("%d", &num);*/
 	
-	 int num= 18;
+	int num= 18;
+	
 	BitMapEntryKey coppia = BitMap_blockToIndex(num,bmap);
 	
 	printf("dato l'indice %d , otteniamo il byte numero: ", num);
@@ -94,6 +105,7 @@ printf("inizio\n\n");
 
 
 //-----------------FINE TEST BITMAP -------------------------
+
 
 
 //------------------TEST DISK_DRIVER-------------------------
