bba2786a85cfe58cfa7043fc011cc3d68f783e27
Sarafa92 <41699020+Sarafa92@users.noreply.github.com>
2019-09-02 18:09:29 +0200

    sostituzione file errato

--- disk_driver.h
+++ disk_driver.h
@@ -1,146 +1,53 @@
-#pragma once
+//#pragma once
 #include "bitmap.h"
-#include "disk_driver.h"
 
-/*these are structures stored on disk*/
-
-// header, occupies the first portion of each block in the disk
-// represents a chained list of blocks
+#define BLOCK_SIZE 512
+// this is stored in the 1st block of the disk
 typedef struct {
-  int previous_block; // chained list (previous block)
-  int next_block;     // chained list (next_block)
-  int block_in_file; // position in the file, if 0 we have a file control block
-} BlockHeader;
+  int num_blocks;
+  int bitmap_blocks;   // how many blocks in the bitmap
+  int bitmap_entries;  // how many bytes are needed to store the bitmap
 
+  int free_blocks;     // free blocks
+  int first_free_block;// first block index
+  int blocchi_riservati; //blocchi riservati per l'allocazione dell'header e della bitmap.
 
-// this is in the first block of a chain, after the header
-typedef struct {
-  int directory_block; // first block of the parent directory
-  int block_in_disk;   // repeated position of the block on the disk
-  char name[128];
-  int  size_in_bytes;
-  int size_in_blocks;
-  int is_dir;          // 0 for file, 1 for dir
-} FileControlBlock;
-
-// this is the first physical block of a file
-// it has a header
-// an FCB storing file infos
-// and can contain some data
-
-/******************* stuff on disk BEGIN *******************/
-typedef struct {
-  BlockHeader header;
-  FileControlBlock fcb;
-  char data[BLOCK_SIZE-sizeof(FileControlBlock) - sizeof(BlockHeader)] ;
-} FirstFileBlock;
-
-// this is one of the next physical blocks of a file
-typedef struct {
-  BlockHeader header;
-  char  data[BLOCK_SIZE-sizeof(BlockHeader)];
-} FileBlock;
-
-// this is the first physical block of a directory
-typedef struct {
-  BlockHeader header;
-  FileControlBlock fcb;
-  int num_entries;
-  int file_blocks[ (BLOCK_SIZE
-		   -sizeof(BlockHeader)
-		   -sizeof(FileControlBlock)
-		    -sizeof(int))/sizeof(int) ];
-} FirstDirectoryBlock;
-
-// this is remainder block of a directory
-typedef struct {
-  BlockHeader header;
-  int file_blocks[ (BLOCK_SIZE-sizeof(BlockHeader))/sizeof(int) ];
-} DirectoryBlock;
-/******************* stuff on disk END *******************/
-
-
-
-
-  
-typedef struct {
-  DiskDriver* disk;
-  int blocchi_riservati;
-  // add more fields if needed
-} SimpleFS;
-
-// this is a file handle, used to refer to open files
-typedef struct {
-  SimpleFS* sfs;                   // pointer to memory file system structure
-  FirstFileBlock* fcb;             // pointer to the first block of the file(read it)
-  FirstDirectoryBlock* directory;  // pointer to the directory where the file is stored
-  BlockHeader* current_block;      // current block in the file
-  int pos_in_file;                 // position of the cursor
-} FileHandle;
+} DiskHeader;
 
 typedef struct {
-  SimpleFS* sfs;                   // pointer to memory file system structure
-  FirstDirectoryBlock* dcb;        // pointer to the first block of the directory(read it)
-  FirstDirectoryBlock* directory;  // pointer to the parent directory (null if top level)
-  BlockHeader* current_block;      // current block in the directory
-  int pos_in_dir;                  // absolute position of the cursor in the directory
-  int pos_in_block;                // relative position of the cursor in the block
-} DirectoryHandle;
+  DiskHeader* header; // mmapped
+  char* bitmap_data;  // mmapped (bitmap)
+  int fd; // for us
+} DiskDriver;
 
-// initializes a file system on an already made disk
-// returns a handle to the top level directory stored in the first block
-DirectoryHandle* SimpleFS_init(SimpleFS* fs, DiskDriver* disk);
+/**
+   The blocks indices seen by the read/write functions
+   have to be calculated after the space occupied by the bitmap
+*/
 
-// creates the inital structures, the top level directory
-// has name "/" and its control block is in the first position
-// it also clears the bitmap of occupied blocks on the disk
-// the current_directory_block is cached in the SimpleFS struct
-// and set to the top level directory
-void SimpleFS_format(SimpleFS* fs, char* filename, int num_blocks);
+// opens the file (creating it if necessary_
+// allocates the necessary space on the disk
+// calculates how big the bitmap should be
+// if the file was new
+// compiles a disk header, and fills in the bitmap of appropriate size
+// with all 0 (to denote the free space);
+void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks);
 
-// creates an empty file in the directory d
-// returns null on error (file existing, no free blocks)
-// an empty file consists only of a block of type FirstBlock
-FileHandle* SimpleFS_createFile(DirectoryHandle* d, const char* filename);
+// reads the block in position block_num
+// returns -1 if the block is free accrding to the bitmap
+// 0 otherwise
+int DiskDriver_readBlock(DiskDriver* disk, void* dest, int block_num);
 
-// reads in the (preallocated) blocks array, the name of all files in a directory 
-int SimpleFS_readDir(char** names, DirectoryHandle* d);
+// writes a block in position block_num, and alters the bitmap accordingly
+// returns -1 if operation not possible
+int DiskDriver_writeBlock(DiskDriver* disk, void* src, int block_num);
 
+// frees a block in position block_num, and alters the bitmap accordingly
+// returns -1 if operation not possible
+int DiskDriver_freeBlock(DiskDriver* disk, int block_num);
 
-// opens a file in the  directory d. The file should be exisiting
-FileHandle* SimpleFS_openFile(DirectoryHandle* d, const char* filename);
+// returns the first free blockin the disk from position (checking the bitmap)
+int DiskDriver_getFreeBlock(DiskDriver* disk, int start);
 
-
-// closes a file handle (destroyes it)
-int SimpleFS_close(FileHandle* f);
-
-// writes in the file, at current position for size bytes stored in data
-// overwriting and allocating new space if necessary
-// returns the number of bytes written
-int SimpleFS_write(FileHandle* f, void* data, int size);
-
-// writes in the file, at current position size bytes stored in data
-// overwriting and allocating new space if necessary
-// returns the number of bytes read
-int SimpleFS_read(FileHandle* f, void* data, int size);
-
-// returns the number of bytes read (moving the current pointer to pos)
-// returns pos on success
-// -1 on error (file too short)
-int SimpleFS_seek(FileHandle* f, int pos);
-
-// seeks for a directory in d. If dirname is equal to ".." it goes one level up
-// 0 on success, negative value on error
-// it does side effect on the provided handle
- int SimpleFS_changeDir(DirectoryHandle* d, char* dirname);
-
-// creates a new directory in the current one (stored in fs->current_directory_block)
-// 0 on success
-// -1 on error
-int SimpleFS_mkDir(DirectoryHandle* d, char* dirname);
-
-// removes the file in the current directory
-// returns -1 on failure 0 on success
-// if a directory, it removes recursively all contained files
-int SimpleFS_remove(SimpleFS* fs, char* filename);
-
+// writes the data (flushing the mmaps)
+int DiskDriver_flush(DiskDriver* disk);
